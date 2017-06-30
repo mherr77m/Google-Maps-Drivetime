6a89436198a71c45532aee515333e40c29c6603c
Michael Herrera <mherrera@boreas.nrl.navy.mil>
2017-06-30 14:26:13 -0400

    Removed API Key

--- drivetime.py
+++ drivetime.py
@@ -5,7 +5,7 @@ import urllib.request
 save_fn = 'path/drivetimes.txt'
 origin = '1600+Pennsylvania+Ave+NW+Washington+DC+20006'
 destination = '1451+Broadway+New+York+NY+10036'
-APIkey = 'AIzaSyDDzhn_iQznGgyd5LyyTjkBe-O3lVxpkV8'
+APIkey = ''
 traffic_model = 'best_guess'
 
 url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&departure_time=now&traffic_model={2}&key={3}".format(origin,destination,traffic_model,APIkey)
